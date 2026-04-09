bd2d42f8ab7fbbdeff19b71e306e8db0f8808b4e
joaopaulobnf-ai <joaopaulo.bnf@gmail.com>
2026-04-09 13:15:08 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
-import "dotenv/config";
 import app from "./app.js";
-import { env } from "./config/env.js";
 
-app.listen(env.port, () => {
-  console.log(`Servidor rodando em http://localhost:${env.port}`);
+const PORT = Number(process.env.PORT) || 10000;
+const HOST = "0.0.0.0";
+
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor rodando em http://${HOST}:${PORT}`);
 });
