b3ad681251c95790cddad047ff22286f4b4783ee
joaopaulobnf-ai <joaopaulo.bnf@gmail.com>
2026-04-10 08:22:00 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const server = app.listen(PORT, HOST, () => {
   console.log(`Servidor rodando em http://${HOST}:${PORT}`);
 });
 
-server.on("error", (error) => {
+server.on("error", error => {
   console.error("Erro ao iniciar servidor:", error);
   process.exit(1);
 });
